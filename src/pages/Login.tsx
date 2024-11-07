3415461cac408186736f567745fea257f3e324a1
DiegoAle6 <diegoalemanmena@gmail.com>
2024-11-07 01:44:21 -0500

    Ruteo y header

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,1 +1,11 @@
-//Login;
+// pages/Login.tsx
+const Login = () => {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">Iniciar Sesión</h1>
+        {/* Contenido de Login */}
+      </div>
+    )
+  }
+  
+  export default Login
